7231adbf258e1639300bbcf60577b7064a82796b
Jonathan Trowbridge <jtrowbridge@costar.com>
2021-07-19 16:27:11 -0400

    Simplified paging

--- app/src/main/java/com/example/pagingpoc/common/paging/PagingCacheRemoteMediator.kt
+++ app/src/main/java/com/example/pagingpoc/common/paging/PagingCacheRemoteMediator.kt
@@ -20,24 +20,14 @@ class PagingCacheRemoteMediator<Key : Any, Item : Any, ItemId : Any>(
         loadType: LoadType,
         state: PagingState<Key, Item>
     ): MediatorResult {
-        val key = when (loadType) {
-            LoadType.REFRESH -> {
-                val pageKey = getRemoteKeyClosestToCurrentPosition(state)
-                pageKey?.nextPageKey ?: startingPageKey
-            }
-            LoadType.PREPEND -> {
-                val pageKey = getRemoteKeyForFirstItem(state)
-                val prevKey = pageKey?.previousPageKey
-                    ?: return MediatorResult.Success(endOfPaginationReached = pageKey != null)
-
-                prevKey
-            }
-            LoadType.APPEND -> {
-                val pageKey = getRemoteKeyForLastItem(state)
-                val nextKey = pageKey?.nextPageKey
-                    ?: return MediatorResult.Success(endOfPaginationReached = pageKey != null)
-                nextKey
-            }
+        val key: Key = when (loadType) {
+            LoadType.REFRESH -> startingPageKey
+            LoadType.PREPEND -> return MediatorResult.Success(endOfPaginationReached = true)
+            LoadType.APPEND -> state
+                .lastItemOrNull()
+                ?.let { pagingCache.getPageKeyForItemId(itemIdResolver.getId(it)) }
+                ?.nextPageKey
+                ?: return MediatorResult.Success(endOfPaginationReached = true)
         }
 
         return try {
@@ -60,8 +50,6 @@ class PagingCacheRemoteMediator<Key : Any, Item : Any, ItemId : Any>(
         }.also { result: MediatorResult ->
             Log.d(TAG, "--------")
             Log.d(TAG, "Mediator request load type: $loadType")
-//            Log.d(TAG, "Mediator request state: $state")
-//            Log.d(TAG, "Mediator request result: $result")
 
             if (result is MediatorResult.Success) {
                 Log.d(
@@ -73,30 +61,6 @@ class PagingCacheRemoteMediator<Key : Any, Item : Any, ItemId : Any>(
             }
 
         }
-    }
-
-    private fun getRemoteKeyClosestToCurrentPosition(state: PagingState<Key, Item>): PageKey<Key>? {
-        val anchorPosition = state.anchorPosition ?: return null
-        val closetItem = state.closestItemToPosition(anchorPosition) ?: return null
-        val itemId = itemIdResolver.getId(closetItem)
-
-        return pagingCache.getPageKeyForItemId(itemId)
-    }
-
-    private fun getRemoteKeyForFirstItem(state: PagingState<Key, Item>): PageKey<Key>? {
-        val firstPage = state.pages.firstOrNull { it.data.isNotEmpty() } ?: return null
-        val firstItem = firstPage.data.firstOrNull() ?: return null
-        val itemId = itemIdResolver.getId(firstItem)
-
-        return pagingCache.getPageKeyForItemId(itemId)
-    }
-
-    private fun getRemoteKeyForLastItem(state: PagingState<Key, Item>): PageKey<Key>? {
-        val lastPage = state.pages.lastOrNull() { it.data.isNotEmpty() } ?: return null
-        val lastItem = lastPage.data.lastOrNull() ?: return null
-        val itemId = itemIdResolver.getId(lastItem)
-
-        return pagingCache.getPageKeyForItemId(itemId)
     }
 
     companion object {
